c00fd340a9ffa81af396a8a5630c76f4df705301
babludangi6266 <babludangi2000@gmail.com>
2026-04-06 17:24:10 +0530

    feat: initialize axios instance with JWT authentication interceptor

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
-  //baseURL: 'https://crm-backend-k2us.onrender.com/api',
+  //baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://crm-backend-k2us.onrender.com/api',
 
 });
 
